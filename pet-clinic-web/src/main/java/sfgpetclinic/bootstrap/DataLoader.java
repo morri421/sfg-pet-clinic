9e91e6b5f9479a56ee7658b831b3bb10862be586
Devon Morrison <devmorri421@gmail.com>
2019-12-29 15:49:11 -0500

    Modified Thymeleaf template to index for owners for owneer list

--- pet-clinic-web/src/main/java/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/sfgpetclinic/bootstrap/DataLoader.java
@@ -31,9 +31,9 @@ public class DataLoader implements CommandLineRunner {
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner1.setId(2L);
-        owner1.setFirstName("Sam");
-        owner1.setLastName("Naples");
+        owner2.setId(2L);
+        owner2.setFirstName("Sam");
+        owner2.setLastName("Naples");
 
         ownerService.save(owner2);
 
--- pet-clinic-web/src/main/java/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/sfgpetclinic/controllers/OwnerController.java
@@ -2,14 +2,25 @@ package sfgpetclinic.controllers;
 
 import org.hibernate.validator.constraints.pl.REGON;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import sfgpetclinic.services.OwnerService;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOwners() {
+    public String listOwners(Model model) {
+
+        model.addAttribute("owners", ownerService.findAll());
+
         return "owners/index";
     }
 }
