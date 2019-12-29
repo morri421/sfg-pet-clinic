d187966bac23fe46f943d3751665fed0c956377f
Devon Morrison <devmorri421@gmail.com>
2019-12-29 15:58:38 -0500

    Added thymeleaf table to vet index. Fixed list bug in DataLoader

--- pet-clinic-web/src/main/java/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/sfgpetclinic/bootstrap/DataLoader.java
@@ -48,9 +48,9 @@ public class DataLoader implements CommandLineRunner {
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet1.setId(2L);
-        vet1.setFirstName("Vernon");
-        vet1.setLastName("Lalone");
+        vet2.setId(2L);
+        vet2.setFirstName("Vernon");
+        vet2.setLastName("Lalone");
 
         vetService.save(vet2);
 
